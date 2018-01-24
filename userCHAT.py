d808e6c38304d9b3e84c4f39de7c6f6b46677db6
mwohletz489 <33496835+mwohletz489@users.noreply.github.com>
2018-01-24 12:35:47 -0600

    Delete userCHAT.py

--- userCHAT.py
+++
@@ -1,7 +0,0 @@
-from mcpi.minecraft import Minecraft
-mc = Minecraft.create()
-message = "this is the default message. "
-mc.postToChat(message)
-username = input("Please enter a username:")
-message = input("Enter your message: ")
-
